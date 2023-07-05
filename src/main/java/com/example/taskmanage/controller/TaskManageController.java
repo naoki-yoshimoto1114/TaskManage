d896b7cc07c3b7ffb12cb6bd71954ab7500223dc
naoki-yoshimoto1114 <n.yoshimoto1114@gmail.com>
2023-07-05 13:23:03 +0900

    postgresと接続できるように変更

--- src/main/java/com/example/taskmanage/controller/TaskManageController.java
+++ src/main/java/com/example/taskmanage/controller/TaskManageController.java
@@ -1,15 +1,24 @@
 package com.example.taskmanage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class TaskManageController {
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        String sql = "SELECT * FROM tasks";
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(sql);
+        model.addAttribute("tasks", result);
         return "index";
     }
 }
