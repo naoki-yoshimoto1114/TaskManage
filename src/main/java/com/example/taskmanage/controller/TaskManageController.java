4e28f3364d034e742228385ff8e12475e4ab1bb8
naoki-yoshimoto1114 <n.yoshimoto1114@gmail.com>
2023-07-19 12:20:26 +0900

    2023/07/19授業追加

--- src/main/java/com/example/taskmanage/controller/TaskManageController.java
+++ src/main/java/com/example/taskmanage/controller/TaskManageController.java
@@ -1,11 +1,16 @@
 package com.example.taskmanage.controller;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +19,11 @@ public class TaskManageController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    /**
+     * タスクの一覧表示
+     * @param model
+     * @return
+     */
     @GetMapping("/")
     public String index(Model model) {
         String sql = "SELECT * FROM tasks";
@@ -23,7 +33,50 @@ public class TaskManageController {
     }
 
     @GetMapping("/regist")
-    public String task_regist() {
+    public String regist() {
         return "regist";
+    }
+
+    /**
+     * タスクの新規登録
+     * @param name
+     * @param dead_line
+     * @param planned_time
+     * @param status
+     * @return
+     */
+    @PostMapping("/create")
+    public String create(@RequestParam("name") String name,
+                         @RequestParam("dead_line") String dead_line,
+                         @RequestParam("planned_time") String planned_time,
+                         @RequestParam("status") String status) {
+
+        LocalDate deadLineDate = LocalDate.parse(dead_line);
+        int plannedTime = Integer.parseInt(planned_time);
+        int statusCode = Integer.parseInt(status);
+
+        String sql = "INSERT INTO tasks (name, dead_line, planned_time, status) "
+                    + " VALUES(?, ?, ?, ?)";
+
+        jdbcTemplate.update(sql, name, deadLineDate, plannedTime, statusCode);
+
+        return "index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id) {
+        return "redirect: /index";
+    }
+
+    /**
+     * タスクの削除
+     * @param id
+     * @return
+     */
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id) {
+        String sql = "DELETE FROM tasks WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+        return "redirect: /index";
     }
 }
